3fd491482da6484b944c053e54fb9bf63cd75640
MenbereHailu <Menberehailu35@gmail.com>
2025-05-20 04:57:37 -0700

    feat: basic Streamlit UI

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def load_data():
-    benin = pd.read_csv('data/benin_clean.csv')
-    togo = pd.read_csv('data/togo_clean.csv')
-    sierra_leone = pd.read_csv('data/sierra_leone_clean.csv')
+    benin = pd.read_csv('C:/Users/hp/OneDrive - menbimarket/Desktop/solar-challenge-week1/data/Benin_clean.csv')
+    togo = pd.read_csv('C:/Users/hp/OneDrive - menbimarket/Desktop/solar-challenge-week1/data/Togo_clean.csv')
+    sierra_leone = pd.read_csv('C:/Users/hp/OneDrive - menbimarket/Desktop/solar-challenge-week1/data/sierraleone_clean.csv')
 
     benin['Country'] = 'Benin'
     togo['Country'] = 'Togo'
