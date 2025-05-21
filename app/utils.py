38147afa422b3da9d15e6ea3b877d23f3db0c862
MenbereHailu <Menberehailu35@gmail.com>
2025-05-20 23:13:15 -0700

    streamline

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def load_data():
-    benin = pd.read_csv('C:/Users/hp/OneDrive - menbimarket/Desktop/solar-challenge-week1/data/Benin_clean.csv')
-    togo = pd.read_csv('C:/Users/hp/OneDrive - menbimarket/Desktop/solar-challenge-week1/data/Togo_clean.csv')
-    sierra_leone = pd.read_csv('C:/Users/hp/OneDrive - menbimarket/Desktop/solar-challenge-week1/data/sierraleone_clean.csv')
+    benin = pd.read_csv('data/Benin_clean.csv')
+    togo = pd.read_csv('Togo_clean.csv')
+    sierra_leone = pd.read_csv('data/sierraleone_clean.csv')
 
     benin['Country'] = 'Benin'
     togo['Country'] = 'Togo'
